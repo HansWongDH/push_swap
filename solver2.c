57cfd440e84c522d579267c96d37a51e19394bf6
Hans Wong <hansdhwong@HANS-DESKTOP.localdomain>
2022-03-22 03:43:38 +0800

    713

--- solver2.c
+++ solver2.c
@@ -6,28 +6,20 @@
 /*   By: wding-ha <wding-ha@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 17:25:18 by wding-ha          #+#    #+#             */
-/*   Updated: 2022/03/22 02:27:45 by wding-ha         ###   ########.fr       */
+/*   Updated: 2022/03/22 03:42:39 by wding-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap2.h"
 
-void	printlist(t_stack *list)
+void	push_number(t_stack **lsta, t_stack **lstb, t_add **info)
 {
-		while (list)
-	{
-		printf("Linked list %d :%d \n", list->stk, list->num);
-		list = list->next;
-	}
-	printf("--------------------\n");
-}
-
-void	push_number(t_stack **lsta, t_stack **lstb)
-{
-	while (*lsta)
+	while ((*info)->len > 0)
 	{
 		p_ab(lsta, lstb);
+		(*info)->len--;
 	}
+	delete_add(info);
 }
 
 t_stack	*partition_a(t_stack **stka, t_stack **stkb, t_add **a)
@@ -55,11 +47,15 @@ t_stack	*partition_a(t_stack **stka, t_stack **stkb, t_add **a)
 		len--;
 	}
 	while (i-- > 0 && (*a)->tail)
+	{
+		if ((*stka)->num == pivot)
+			(*a)->len -= p_ab(stka, stkb);
 		rr_ab(stka);
+	}
 	return (tail);
 }
 
-t_stack	*partition_b(t_stack **stka, t_stack **stkb, t_add **a)
+t_stack	*partition_b(t_stack **stka, t_stack **stkb, t_add **b)
 {
 	t_stack	*tail;
 	t_stack	*last;
@@ -67,8 +63,8 @@ t_stack	*partition_b(t_stack **stka, t_stack **stkb, t_add **a)
 	int		len;
 	int		pivot;
 
-	last = ft_iter_stop(*stka, (*a)->tail);
-	len = (*a)->len;
+	last = ft_iter_stop(*stka, (*b)->tail);
+	len = (*b)->len;
 	pivot = find_median(*stka, len);
 	i = 0;
 	if (*stkb)
@@ -78,12 +74,12 @@ t_stack	*partition_b(t_stack **stka, t_stack **stkb, t_add **a)
 	while (len > 0)
 	{
 		if ((*stka)->num >= pivot)
-			(*a)->len -= p_ab(stka, stkb);
+			(*b)->len -= p_ab(stka, stkb);
 		else if (*stka != last)
 			i += r_ab(stka);
 		len--;
 	}
-	while (i-- > 0 && (*a)->tail)
+	while (i-- > 0 && (*b)->tail)
 		rr_ab(stka);
 	return (tail);
 }
@@ -98,14 +94,14 @@ void	sorting(t_stack **stka, t_stack **stkb, t_add **a, t_add **b)
 	if ((*stka)->stk == 1)
 	{
 		tailb = partition_a(stka, stkb, a);
-		if ((*a)->len == 0)
+		if (*a && (*a)->len == 0)
 			delete_add(a);
 		ft_add_front(b, ft_new_add(tailb, ft_stacksize_n(*stkb, tailb)));
 	}
 	if ((*stka)->stk == 2)
 	{
 		taila = partition_b(stka, stkb, b);
-		if ((*b)->len == 0)
+		if (*b && (*b)->len == 0)
 			delete_add(b);
 		ft_add_front(a, ft_new_add(taila, ft_stacksize_n(*stkb, taila)));
 	}
@@ -113,22 +109,23 @@ void	sorting(t_stack **stka, t_stack **stkb, t_add **a, t_add **b)
 
 void	sort(t_stack **stka, t_stack **stkb, t_add **a, t_add **b)
 {
-	if (!issorted(*stka) && ft_stacksize_n(*stka, (*a)->tail) > 3)
-		sorting(stka, stkb, a, b);
-	else if (!issorted(*stka) && ft_stacksize_n(*stka, (*a)->tail) == 3)
-		three_case_part(stka, (*a)->tail);
-	else if (!issorted(*stka) && ft_stacksize_n(*stka, (*a)->tail) == 2)
-		s_ab(stka);
+	if (!issorted(*stka))
+	{
+		if ((*a)->len > 3)
+			sorting(stka, stkb, a, b);
+		else if ((*a)->len == 3)
+			three_case_part(stka, (*a)->tail);
+		else if ((*a)->len == 2)
+			s_ab(stka);
+	}
 	if (issorted(*stka) && *stkb)
 	{
-		//if (!issorted(*stkb))
+		if (!issorted(*stkb) && (*b)->len > 2)
 			sorting(stkb, stka, a, b);
-	//	else if (!issorted(*stkb) && (*b)->len == 2)
-		//	s_ab(stkb);
-		//else
-		//	push_number(stkb, stka);
-	//	else if ((*b)->len == 2)
-	//		s_ab(stkb);
+		else if (!issortedtail(*stkb, 2) && (*b)->len == 2)
+			s_ab(stkb);
+		else
+			push_number(stkb, stka, b);
 	}
 	if (!issorted(*stka) || *stkb)
 		sort(stka, stkb, a, b);
